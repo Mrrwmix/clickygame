abf07031c70c04c14a2e2f71585cfab5f0f9769a
Mrrwmix <seidoc@gmail.com>
2019-09-11 17:55:16 -0700

    Sort randomization added

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,15 +1,17 @@
-import React, { useContext } from 'react';
-import GameContext from '../context/gameContext';
-import Image from './Image';
+import React, { useContext } from "react";
+import GameContext from "../context/gameContext";
+import Image from "./Image";
 
 const ImageContainer = () => {
   const gameContext = useContext(GameContext);
   const { images } = gameContext;
   return (
     <div className='flex-row mx-4 d-flex justify-content-between flex-wrap'>
-      {images.map(item => (
-        <Image image={item} key={item.id} />
-      ))}
+      {images
+        .sort((a, b) => (a.random >= b.random ? 1 : -1))
+        .map(item => (
+          <Image image={item} key={item.id} />
+        ))}
     </div>
   );
 };
