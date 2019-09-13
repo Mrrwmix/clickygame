fa2d9f1757a74ef9984f65b8a16b90dda1778ff6
Mrrwmix <seidoc@gmail.com>
2019-09-13 16:33:49 -0700

    Updates

---
+++ precache-manifest.349bfd0596418f57d71272cf51266d00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c67fa459ee0ae0231983b86efa49274",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "25cf3acd26facf244faf",
+    "url": "/clickygame/static/css/main.d2c237c2.chunk.css"
+  },
+  {
+    "revision": "b4bc869285d316361af8",
+    "url": "/clickygame/static/js/2.1fa7cc78.chunk.js"
+  },
+  {
+    "revision": "25cf3acd26facf244faf",
+    "url": "/clickygame/static/js/main.4caf38de.chunk.js"
+  },
+  {
+    "revision": "7ceb064d0ddef4f14f98",
+    "url": "/clickygame/static/js/runtime~main.dd7f9e86.js"
+  }
+]);
